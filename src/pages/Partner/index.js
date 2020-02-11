5912e0eddde88812ad0430edf34fd3a84f90cd69
Manoel C <manoelclemente9@outlook.com>
2020-02-11 15:45:50 -0300

    create partner

---
+++ src/pages/Partner/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Footer from '../../components/Footer';
+import Header from '../../components/Header';
+
+// import { Container } from './styles';
+
+export default function Partner() {
+  return (
+    <>
+    <h1>About uss</h1>
+    <Header />
+    <Footer />
+
+    </>
+  );
+}
