7b3a1e48d6f3d8c6e0fd615ae9f0ac085d5f96fa
dfealves <daniloferreira.alves@outlook.com>
2020-02-14 12:37:01 -0300

    fix button initial page

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -32,7 +32,21 @@ export default function Main() {
                   textDecoration: 'none',
                 }}
               >
-                <button type="button" className="pl-5 pr-5">
+                <button
+                  id="associe"
+                  type="button"
+                  className="pl-5 pr-5"
+                  style={{
+                    width: '194px',
+                    height: '66.63px',
+                    background: '#00B2F6',
+                    boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)',
+                    borderRadius: '30px',
+                    border: '0',
+                    color: '#fff',
+                    fontSize: '20px',
+                  }}
+                >
                   Associe-se
                 </button>
               </Link>
