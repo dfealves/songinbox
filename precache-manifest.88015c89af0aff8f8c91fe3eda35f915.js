3db00fa4ec59075bd7e074b6e208a6d8b03d84df
dfealves <daniloferreira.alves@outlook.com>
2020-02-14 12:04:56 -0300

    Updates

---
+++ precache-manifest.88015c89af0aff8f8c91fe3eda35f915.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80888b0ac6ac15b197c6811efaa2a7d4",
+    "url": "/songinbox/index.html"
+  },
+  {
+    "revision": "cbb4c57ca9d02a370ee9",
+    "url": "/songinbox/static/css/2.67123b20.chunk.css"
+  },
+  {
+    "revision": "cbb4c57ca9d02a370ee9",
+    "url": "/songinbox/static/js/2.7a8040cd.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/songinbox/static/js/2.7a8040cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d960f9c8a229ffee59dc",
+    "url": "/songinbox/static/js/main.ae88fad6.chunk.js"
+  },
+  {
+    "revision": "b7a768cf4f7190e4a1a0",
+    "url": "/songinbox/static/js/runtime-main.825e0d70.js"
+  },
+  {
+    "revision": "b4ab1e76a8c090bf4fe97818b9cb36bd",
+    "url": "/songinbox/static/media/CasseteBox-mini.b4ab1e76.png"
+  },
+  {
+    "revision": "d714783d7c29c3beec782ca306c2cd22",
+    "url": "/songinbox/static/media/VinilBox-mini.d714783d.png"
+  },
+  {
+    "revision": "d7f638b1049807dd676a5cd9283b1e70",
+    "url": "/songinbox/static/media/caixa_musica.d7f638b1.png"
+  },
+  {
+    "revision": "a245662c142bccd1f9b8d3915f9e7d08",
+    "url": "/songinbox/static/media/image-sobre.a245662c.png"
+  },
+  {
+    "revision": "c1c441e4bbe68491c7177ed831180030",
+    "url": "/songinbox/static/media/img-apresentacao.c1c441e4.png"
+  },
+  {
+    "revision": "25c0484f47efa8b4d8940aa86e21eea7",
+    "url": "/songinbox/static/media/kit-fanzao.25c0484f.png"
+  },
+  {
+    "revision": "8280457170581e947adda81708e57c23",
+    "url": "/songinbox/static/media/kit-fanzinho.82804571.png"
+  },
+  {
+    "revision": "3061ad944dba85cdabf3edfc40c4fd01",
+    "url": "/songinbox/static/media/logo-menu.3061ad94.png"
+  },
+  {
+    "revision": "b8124f2c24d11b341bcd494c5f45438a",
+    "url": "/songinbox/static/media/mulher-musica.b8124f2c.png"
+  },
+  {
+    "revision": "4d67486807daaa4f0ef03916aa500c0c",
+    "url": "/songinbox/static/media/previous-mini.4d674868.png"
+  },
+  {
+    "revision": "7cbd6989752a95ac39cb305e84cb2bf8",
+    "url": "/songinbox/static/media/previous-mobile.7cbd6989.png"
+  },
+  {
+    "revision": "5dc21948036e34476fa1915ffa61ea59",
+    "url": "/songinbox/static/media/previous.5dc21948.png"
+  }
+]);
